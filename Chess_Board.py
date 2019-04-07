631e2f6eef2af54570f82a1ad86feba2ba481983
Amogh R <38137144+amogh2004@users.noreply.github.com>
2019-04-07 22:09:39 +0530

    Add files via upload

--- Chess_Board.py
+++ Chess_Board.py
@@ -1,41 +1,46 @@
 import turtle
 
-turt=turtle.Turtle()
+turt = turtle.Turtle()
 turt.speed(30)
 
-def square(size,change,color):
+
+def square(size, change, color):
     turt.color(color)
     turt.begin_fill()
     for i in range(4):
-      turt.fd(size)
-      turt.lt(90)
+        turt.fd(size)
+        turt.lt(90)
     turt.end_fill()
     turt.fd(size)
 
-def row(size,change,c1,c2):
+
+def row(size, change, c1, c2):
     for i in range(4):
-        if(change==True):
+        if change == True:
             square(size,change,c1)
             square(size,change,c2)
         else:
             square(size,change,c2)
             square(size,change,c1)
 
-def chessboard(size,change,c1,c2):
+
+def chessboard(size, change, c1, c2):
     turt.pu()
-    turt.goto(-(size*4),(size*4))
+    turt.goto(-(size*4), (size*4))
     for i in range(8):
-        row(size,change,c1,c2)
+        row(size, change, c1, c2)
         turt.bk(size*8)
         turt.rt(90)
         turt.fd(size)
         turt.lt(90)
-        if(change==True):
-            change=False
-        else:
-            change=True
 
-chessboard(50,True,'light blue','black')
+        if change == True:
+            change = False
+        else:
+            change = True
+
+
+chessboard(50, True, 'light blue', 'black')
             
 
 turt.done()
